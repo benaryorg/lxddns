7a8e31a2f8bd944bd1249a45f59050834e295a38
benaryorg <binary@benary.org>
2022-05-29 20:29:19 +0000

    dep bump: clap
    Signed-off-by: benaryorg <binary@benary.org>

--- src/bin/lxddns.rs
+++ src/bin/lxddns.rs
@@ -6,18 +6,9 @@ use ::
 	async_std::
 	{
 		fs::remove_file,
-		path::Path,
 		task,
 	},
-	clap::
-	{
-		app_from_crate,
-		crate_authors,
-		crate_description,
-		crate_name,
-		crate_version,
-		Arg,
-	},
+	clap::Parser,
 	log::
 	{
 		error,
@@ -25,85 +16,51 @@ use ::
 	},
 };
 
+// check https://github.com/clap-rs/clap/issues/3221 at a later time
+/// PowerDNS backend to bridge the gap between DNS and LXD.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args
+{
+	/// connection string for the message queue
+	#[clap(short, long, value_name = "AMQP_URL", default_value = "amqp://guest:guest@[::1]:5672", env = "LXDDNS_URL")]
+	url: String,
+
+	/// loglevel to be used, if not specified uses env_logger's auto-detection
+	#[clap(short = 'v', long)]
+	loglevel: Option<String>,
+
+	/// hostmaster to announce in SOA (use dot notation including trailing dot as in hostmaster.example.org.)
+	#[clap(short, long, value_name = "SOA_HOSTMASTER")]
+	hostmaster: String,
+
+	/// domain under which to run (do not forget the trailing dot)
+	#[clap(short, long)]
+	domain: String,
+
+	/// location of the unix domain socket to be created
+	#[clap(short, long, value_name = "SOCKET_PATH",  default_value = "/var/run/lxddns/lxddns.sock")]
+	socket: String,
+
+	/// name of queue to be used for query responses; if not specified uses randomly assigned queue name
+	#[clap(short, long)]
+	queuename: Option<String>,
+
+	/// number of parallel worker threads for message queue responders (0: unlimited)
+	#[clap(long, value_name = "THREAD_COUNT", default_value = "2")]
+	responder_workers: usize,
+
+	/// number of parallel worker threads for unix domain socket connections (0: unlimited)
+	#[clap(long, value_name = "THREAD_COUNT", default_value = "2")]
+	unix_workers: usize,
+}
+
 #[async_std::main]
 async fn main()
 {
-	let matches = app_from_crate!()
-        .arg(Arg::with_name("url")
-            .short("u")
-            .long("url")
-            .help("connection string for the message queue")
-            .takes_value(true)
-            .env("LXDDNS_URL")
-            .value_name("AMQP_URL")
-            .default_value("amqp://guest:guest@[::1]:5672")
-            .multiple(false)
-		)
-        .arg(Arg::with_name("loglevel")
-            .short("v")
-            .long("loglevel")
-            .help("loglevel to be used, if not specified uses env_logger's auto-detection")
-            .takes_value(true)
-            .value_name("LOGLEVEL")
-            .multiple(false)
-		)
-        .arg(Arg::with_name("hostmaster")
-            .short("h")
-            .long("hostmaster")
-            .help("hostmaster to announce in SOA (use dot notation including trailing dot as in hostmaster.example.org.)")
-            .takes_value(true)
-            .value_name("SOA_HOSTMASTER")
-            .multiple(false)
-            .required(true)
-		)
-        .arg(Arg::with_name("domain")
-            .short("d")
-            .long("domain")
-            .help("domain under which to run (do not forget the trailing dot)")
-            .takes_value(true)
-            .value_name("DOMAIN")
-            .multiple(false)
-            .required(true)
-		)
-		.arg(Arg::with_name("socket")
-            .short("s")
-            .long("socket")
-            .help("location of the unix domain socket to be created")
-            .takes_value(true)
-            .value_name("SOCKET_PATH")
-            .default_value("/var/run/lxddns/lxddns.sock")
-            .multiple(false)
-		)
-		.arg(Arg::with_name("queuename")
-            .short("q")
-            .long("queuename")
-            .help("name of queue to be used for query responses; empty string is randomly assigned queue name")
-            .takes_value(true)
-            .value_name("QUEUE_NAME")
-            .default_value("")
-            .multiple(false)
-		)
-		.arg(Arg::with_name("responder-workers")
-            .long("responder-workers")
-            .help("number of parallel worker threads for message queue responders (0: unlimited)")
-            .takes_value(true)
-            .value_name("THREAD_COUNT")
-            .default_value("2")
-            .validator(|value| value.as_str().parse::<usize>().map(|_| ()).map_err(|err| format!("{}", err)))
-            .multiple(false)
-		)
-		.arg(Arg::with_name("unix-workers")
-            .long("unix-workers")
-            .help("number of parallel worker threads for unix domain socket connections (0: unlimited)")
-            .takes_value(true)
-            .value_name("THREAD_COUNT")
-            .default_value("2")
-            .validator(|value| value.as_str().parse::<usize>().map(|_| ()).map_err(|err| format!("{}", err)))
-            .multiple(false)
-		)
-		.get_matches();
+	let args = Args::parse();
 
-	if let Some(loglevel) = matches.value_of("loglevel")
+	if let Some(loglevel) = args.loglevel
 	{
 		std::env::set_var("RUST_LOG", loglevel);
 	}
@@ -116,13 +73,13 @@ async fn main()
 	info!("[main] logging initialised");
 
 	let server = Server::builder()
-		.url(matches.value_of("url").unwrap())
-		.domain(matches.value_of("domain").unwrap())
-		.hostmaster(matches.value_of("hostmaster").unwrap())
-		.unixpath(Path::new(matches.value_of("socket").unwrap()))
-		.queuename(matches.value_of("queuename").unwrap())
-		.responder_workers(matches.value_of("responder-workers").unwrap().parse::<usize>().unwrap())
-		.unix_workers(matches.value_of("unix-workers").unwrap().parse::<usize>().unwrap())
+		.url(args.url)
+		.domain(args.domain)
+		.hostmaster(args.hostmaster)
+		.unixpath(&args.socket)
+		.queuename(args.queuename.unwrap_or_else(|| "".to_string()))
+		.responder_workers(args.responder_workers)
+		.unix_workers(args.unix_workers)
 	;
 
 	info!("[main] running server");
@@ -139,7 +96,7 @@ async fn main()
 			error!("[main] restarting all services");
 		},
 	}
-	let _ = remove_file(matches.value_of("socket").unwrap()).await;
+	let _ = remove_file(args.socket).await;
 	task::sleep(Duration::from_secs(1)).await;
 }
 
